922ae3b9a50e1f15ec32e3b04aeb7551405ae3af
Swirlds Automation <swirlds-eng-automation@swirlds.com>
2022-08-04 23:11:39 +0000

    Open Review Release v0.26.4-orvw.2
    Signed-off-by: Nathan Klick <nathan@swirldslabs.com>

--- swirlds-common/src/main/java/com/swirlds/common/merkle/synchronization/utility/MerkleSynchronizationException.java
+++ swirlds-common/src/main/java/com/swirlds/common/merkle/synchronization/utility/MerkleSynchronizationException.java
@@ -1,15 +1,17 @@
 /*
  * Copyright 2016-2022 Hedera Hashgraph, LLC
  *
- * This software is owned by Hedera Hashgraph, LLC, which retains title to the software. This software is protected by various
- * intellectual property laws throughout the world, including copyright and patent laws. This software is licensed and
- * not sold. You must use this software only in accordance with the terms of the Hashgraph Open Review license at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * https://github.com/hashgraph/swirlds-open-review/raw/master/LICENSE.md
+ *     https://www.apache.org/licenses/LICENSE-2.0
  *
- * HEDERA HASHGRAPH MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF THIS SOFTWARE, EITHER EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE,
- * OR NON-INFRINGEMENT.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.swirlds.common.merkle.synchronization.utility;
